a8ddbb2f74013191b9750107c05af8525e1b2a46
James gitere <gitere.dev@gmail.com>
2025-03-30 20:17:50 +0300

    updated

--- book-vise/client/src/features/auth/authSlice.js
+++ book-vise/client/src/features/auth/authSlice.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-const apiUrl = "https://bookwise-server-omega.vercel.app/";
+const apiUrl = "https://bookwise-server-omega.vercel.app";
 
 
 const initialState = {
